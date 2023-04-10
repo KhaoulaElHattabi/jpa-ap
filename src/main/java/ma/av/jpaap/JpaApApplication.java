506d0c73cfbcf4e2aef17e986bcdb5fa8d6e26ff
KhaoulaElHattabi <khaoulaelhattabi00@gmail.com>
2023-04-10 03:00:49 +0000

    first commit

---
+++ src/main/java/ma/av/jpaap/JpaApApplication.java
@@ -0,0 +1,13 @@
+package ma.av.jpaap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApApplication.class, args);
+    }
+
+}
